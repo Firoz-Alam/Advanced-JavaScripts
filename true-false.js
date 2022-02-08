1241aa4bb62cd664a5921db07def3a1677f38b23
Firoz-Alam <51852342+Firoz-Alam@users.noreply.github.com>
2022-02-08 22:31:40 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// falsy
+
+// 0
+// ""
+//null
+//undefined
+//NaN
+
+//Truthy
+
+// "0", " ",[],{}
+
+const number = "";
+if(number){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
