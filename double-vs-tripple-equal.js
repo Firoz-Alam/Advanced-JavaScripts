013d8440be1a89e56520e8863e5ac8dd080bd71f
Firoz-Alam <51852342+Firoz-Alam@users.noreply.github.com>
2022-02-09 13:45:52 +0600

    double tripple equal example

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,17 @@
+let num1 = 2;
+let num2 = "2";
+// double equal check only value
+if (num1 == num2) {
+    console.log('condition is true'); // output
+}else{
+    console.log('condition is false');
+}
+
+let num1 = 2;
+let num2 ="2";
+// triple equal check value and type
+if (num1 === num2) {
+    console.log('condition is true');
+}else{
+    console.log('condition is false'); // output
+}
